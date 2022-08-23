58d77abd95dd794b6aad819a22e99f5fb7a77bce
Andrew1397 <andriy.pleskach@gmail.com>
2022-08-23 09:16:53 +0300

    Intial comnit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+      <header className="header">
+        <img src="https://cdn.pixabay.com/photo/2017/03/19/20/19/ball-2157465__340.png"></img>
+      </header>
+  );
+};
+
+export default Header;
