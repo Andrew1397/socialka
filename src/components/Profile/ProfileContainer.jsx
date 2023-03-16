982d024e84ad9ade01f14433c92948c06772e947
Andrew1397 <andriy.pleskach@gmail.com>
2023-03-16 13:27:25 +0200

    fix myProfile + lern useLocate, useNavigate

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -11,29 +11,29 @@ import { withAuthRedirect } from '../hoc/withAuthRedirect';
 // import { setUserProfile } from '../../redux/profile-reducer';
 
 
-// class ProfileContainer extends React.Component {
+// // class ProfileContainer extends React.Component {
 
-//     componentDidMount() {
-//         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-//             .then(response => {
+// //     componentDidMount() {
+// //         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+// //             .then(response => {
 
-//                 this.props.setUserProfile(response.data)
-//             })
-//     }
+// //                 this.props.setUserProfile(response.data)
+// //             })
+// //     }
 
-//     render() {
-//         return (
-//             <Profile {...this.props} profile={this.props.profile} />
-//         )
-//     }
-// }
+// //     render() {
+// //         return (
+// //             <Profile {...this.props} profile={this.props.profile} />
+// //         )
+// //     }
+// // }
 
-// let mapStateToProps = (state) => {
-//     return {
-//         profile: state.profilePage.profile
-//     }
-// }
-// export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
+// // let mapStateToProps = (state) => {
+// //     return {
+// //         profile: state.profilePage.profile
+// //     }
+// // }
+// // export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
 
 
 
@@ -97,3 +97,38 @@ const AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
 
 export default AuthRedirectComponent;
+
+// import React, { useEffect } from 'react';
+// import Profile from './Profile';
+// import { useLocation, useNavigate } from "react-router-dom" // імпортуємо хуки useLocation і useNavigate з react-router-dom
+// import { getUserProfile } from '../../redux/profile-reducer'; // імпортуємо екшн-кріейтор для отримання профілю користувача
+// import Preloader from '../common/Preloader/Preloader'; // імпортуємо компонент Preloader для відображення завантаження
+// import { useDispatch, useSelector } from 'react-redux'; // імпортуємо хуки useDispatch і useSelector з react-redux
+// import { withAuthRedirect } from '../hoc/withAuthRedirect'; // імпортуємо вищий компонент withAuthRedirect для перевірки авторизації
+
+// function ProfileContainer(props) {
+//   const location = useLocation(); // отримуємо об'єкт location з url
+//   const navigate = useNavigate(); // отримуємо функцію navigate для навігації
+//   const userId = location.pathname.split('/')[2]; // витягуємо userId з url за допомогою розбиття рядка на масив
+//   const profile = useSelector(state => state.profilePage.profile); // отримуємо дані про профіль користувача з redux store за допомогою хука useSelector
+//   const isFetching = useSelector(state => state.usersPage.isFetching); // отримуємо статус завантаження даних з redux store за допомогою хука useSelector
+//   const dispatch = useDispatch(); // отримуємо функцію dispatch з redux store за допомогою хука useDispatch
+
+//   useEffect(() => {
+//    if (!userId) {
+//             navigate('/profile/16371'); // якщо userId не вказано, то перенаправляємо на сторінку профілю за замовчуванням за допомогою функції navigate
+//         } else {
+//             dispatch(getUserProfile(userId)); // якщо userId вказано, то викликаємо екшн-кріейтор getUserProfile з userId як параметром і передаємо його в функцію dispatch
+//         }
+//   }, [dispatch, userId, navigate]); // оновлюємо ефект при зміні dispatch, userId або navigate
+
+//   return (
+//     <>
+//       {isFetching ? <Preloader /> : <Profile profile={profile} />} 
+//       {/* // якщо isFetching - правда, то рендеримо компонент Preloader, якщо ні - то рендеримо компонент Profile з пропсом profile */}
+//     </>
+//   );
+// }
+// const AuthRedirectComponent = withAuthRedirect(ProfileContainer); // обгортаємо компонент ProfileContainer вищим компонентом withAuthRedirect для перевірки авторизації
+
+// export default AuthRedirectComponent; // експортуємо результат обгортання як компонент за замовчуванням
